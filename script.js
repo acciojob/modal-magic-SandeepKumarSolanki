cc749ca55ba2742490bad0dd81bfb22237bfec28
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-26 01:07:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+let openModal = document.getElementsByClassName('modal');
+let modalBody = document.getElementsByClassName('modal-body')
+let closeBtn = document.getElementsByClassName('close-modal');
+
+openModal.addEventListener('click' , ()=>{
+	modalBody.style.display='block';
+})
+
+function closeModal() {
+	modalBody.style.display='none';
+}
+
+closeBtn.addEventListener('click',closeModal);
+
+window.addEventListner('click', (e)=>{
+	if(e.target === modal){
+		closeModal();
+	}
+})
